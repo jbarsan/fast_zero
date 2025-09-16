5e899fa48b93ed42cf8eb82a771e3cd14fbf0f7b
João Carlos Barsan <joao_barsan@outlook.com>
2025-09-16 17:28:31 -0300

    .

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,15 +1,28 @@
 from http import HTTPStatus
+
 from fastapi import FastAPI
 
-from fast_zero.schemas import Message, UserSchema, UserPublic
+from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema, UserList
 
 app = FastAPI()
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
 
+
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    ...
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
